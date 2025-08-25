d74e1521ebffec62da79d44842c29597f4e33e3c
jch4835 <jch4835@naver.com>
2025-08-25 13:58:05 +0900

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using RetirePlanner;
+using System.Windows;
+
+namespace RetirePlanner
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = new RetirementPlannerViewModel(); // 한 줄로 끝
+        }
+    }
+}
